76cae59c777810d81f549a2d1e4c0e4f9bacdf41
shaq5035 <shaq5035@gmail.com>
2019-03-30 11:45:23 -0400

    hello

---
+++ src/com/class13/Revirse.java
@@ -0,0 +1,15 @@
+package com.class13;
+
+public class Revirse {
+
+	public static void main(String[] args) {
+// task 1
+		String str ="Sunday";
+		for(int i=str.length()-1;i>=0;i--) {
+			
+				System.out.print(str.charAt(i));
+			
+		}		
+	}
+
+}
